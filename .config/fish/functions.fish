be75752ab7298f697b0b83e5f97e57e5e01a8129
jayka <ajaykaarthick@yahoo.com>
2020-05-21 14:31:04 +0530

    added utils for java

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -30,6 +30,38 @@ function nvm
 	bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
 
+function setjavahome --description 'Set JAVA_HOME'
+  set -l installed_javas  (/usr/libexec/java_home -V 2>&1 1>/dev/null | cut -f3 -s )
+
+  printf "Found %s java installations.\n" (count $installed_javas)
+  
+  for i in (seq 1 (count $installed_javas))
+    printf "%s) %s\n" $i $installed_javas[$i]
+  end
+
+  echo ""
+  read -l -p "echo 'JAVA_HOME (default 1)>' " choice
+
+  if test -z $choice -o $choice -gt (count $installed_javas) -o $choice -lt 1
+    echo "Invalid choice $choice"
+    echo "JAVA_HOME is $JAVA_HOME"
+    return 1
+  end
+
+  set -x JAVA_HOME $installed_javas[$choice]
+
+end
+
+function mvn --description 'Alias to Maven with option to set JAVA_HOME'
+  if test -z "$JAVA_HOME"
+    setjavahome
+  end
+
+  echo "JAVA_HOME=$JAVA_HOME"
+
+  command mvn $argv
+end
+
 function spacemacs --description 'Open Spacemacs'
 	rm ~/.emacs.d
   ln -s ~/setup/.emacs.d ~/.emacs.d
@@ -40,4 +72,37 @@ function vanilaemacs
   rm ~/.emacs.d
   ln -s ~/vanilaemacs ~/.emacs.d
   open -n ~/Applications/Emacs.app
+end
+
+function progress --description "Show progress utility function"
+  set t $argv[1]
+  set total $argv[2]
+  set prg (string repeat -n (math "round(100*$t/$total)") -N "#")
+  
+  echo -ne "Progress: $prg\r"
+end
+
+function increment --description "Increment a counter"
+  math $argv "+" 1
+end
+
+function split-image --description "Split an image in half"
+  
+  read -l -P "Directory to stored the split images (default: cropped): " split_dir
+  if test -z "$split_dir"
+    set split_dir "cropped"
+  end
+
+  if ! test -e $split_dir
+    mkdir "$split_dir"
+  end
+
+  set i 1
+  set total (count $argv)
+  for f in $argv
+    progress $i $total
+    set i (increment $i)
+    cp "$f" "./$split_dir/"
+    mogrify -crop "50%x100%" "./$split_dir/$f"
+  end
 end
--- .config/fish/paths.fish
+++ .config/fish/paths.fish
@@ -1,6 +1,13 @@
-
+set OS (uname)
 # Go
 set -xg GOPATH ~/repo/go
 
 # NVM
 set -xg NVM_DIR ~/.nvm
+
+if test "$OS" = "Darwin" ;
+  set -xg JAVA_HOME (/usr/libexec/java_home)
+end
+
+
+#set -xg PATH $JAVA_HOME/bin:$PATH
