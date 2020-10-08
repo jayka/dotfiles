b5d6bc21d4922dc9a7bfb177eae9072c3ef1979f
jayka <ajaykaarthick@yahoo.com>
2020-10-08 11:30:43 +0530

    updated brew path

--- .config/fish/paths.fish
+++ .config/fish/paths.fish
@@ -9,5 +9,7 @@ if test "$OS" = "Darwin" ;
   set -xg JAVA_HOME (/usr/libexec/java_home)
 end
 
+# brew appdir location
+set -xg HOMEBREW_CASK_OPTS "--appdir=~/Applications --fontdir=/Library/Fonts"
 
 #set -xg PATH $JAVA_HOME/bin:$PATH
